f28a568909bc89c25a6589a7004368383b49eb8a
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2018-12-10 21:05:37 -0200

    WIP

--- LSystem.py
+++ LSystem.py
@@ -1,14 +1,166 @@
 from OpenGL.GLUT import *
-from OpenGL.GLU import *
-from OpenGL.GL import *
+from OpenGL.GLU  import *
+from OpenGL.GL   import *
+from math        import cos, sin, pi
 
-def LSystem():
-    pass
+SavedX = 0
+SavedY = 0
+SavedZ = 0
+
+SavedVectorX = 0
+SavedVectorY = 0
+SavedVectorZ = 0
+
+currentX = 0
+currentY = 0
+currentZ = 0
+
+vectorX = 0
+vectorY = 0
+vectorZ = 0
+
+angleLR = 90
+angleUD = 90
+
+def AddRules(rawRules):
+    rules = {}
+    # Iterates over each rule found in the received list
+    for rule in rawRules:
+        # We split the current rule in two
+        splittedRule = rule.split("->")
+        # And add both parts to a dictionary
+        rules[splittedRule[0].strip()] = splittedRule[1].strip()
+
+    return rules
+
+def LSystem(axiom, rules, recursion):
+    SetCoord()
+
+    # This variable will say what we should display in each iteration
+    display = axiom
+    # Apply the rules n times
+    for i in range(recursion):
+        print(display)
+        Draw(display)
+        aux = ""
+        # Iterates over the current display string
+        for c in display:
+            # If the current char has an rule associated with it we apply it
+            if c in rules.keys():
+                aux += rules[c]
+            # If it does not, we just add it to the new display string
+            else:
+                aux += c
+        display = aux
+
+def SetCoord():
+    global SavedX
+    global SavedY
+    global SavedZ
+    
+    global SavedVectorX
+    global SavedVectorY
+    global SavedVectorZ
+    
+    global currentX
+    global currentY
+    global currentZ
+    
+    global vectorX
+    global vectorY
+    global vectorZ
+
+    global angleLR
+    global angleUD
+
+    SavedX = 0
+    SavedY = 0
+    SavedZ = 0
+    
+    SavedVectorX = 0
+    SavedVectorY = 0
+    SavedVectorZ = 0
+    
+    currentX = 0
+    currentY = 0
+    currentZ = 0
+    
+    vectorX = 0
+    vectorY = 1
+    vectorZ = 0
+
+    angleLR = 0
+    angleUD = 0
+
+def Draw(display):
+    global SavedX
+    global SavedY
+    global SavedZ
+
+    global SavedVectorX
+    global SavedVectorY
+    global SavedVectorZ
+
+    global currentX
+    global currentY
+    global currentZ
+
+    global vectorX
+    global vectorY
+    global vectorZ
+
+    global angleLR
+    global angleUD
+
+    glBegin(GL_LINES)
+
+    # Iterates over the display string and draw what each char represents
+    for letter in display:
+        if letter == 'F':    
+            glVertex3fv([currentX          , currentY          , currentZ])
+            glVertex3fv([currentX + vectorX, currentY + vectorY, currentZ + vectorZ])
+            currentX += vectorX
+            currentY += vectorY
+            currentZ += vectorZ
+        elif letter == 'L':
+            angleLR += 25
+            vectorX  = cos(angleLR*pi/180)
+            vectorY  = sin(angleLR*pi/180)
+        elif letter == 'R':
+            angleLR -= 25
+            vectorX  = cos(angleLR*pi/180)
+            vectorY  = sin(angleLR*pi/180)
+        elif letter == 'U':
+            angleUD += 25
+            vectorZ  = cos(angleUD*pi/180)
+            vectorY  = sin(angleUD*pi/180)
+        elif letter == 'D':
+            angleUD -= 25
+            vectorZ  = cos(angleUD*pi/180)
+            vectorY  = sin(angleUD*pi/180)
+        elif letter == '[':
+            SavedX       = currentX
+            SavedY       = currentY
+            SavedZ       = currentZ
+            SavedVectorX = vectorX
+            SavedVectorY = vectorY
+            SavedVectorZ = vectorZ
+        elif letter == ']':
+            currentX = SavedX
+            currentY = SavedY
+            currentZ = SavedZ
+            vectorX  = SavedVectorX
+            vectorY  = SavedVectorY
+            vectorZ  = SavedVectorZ
+        else:
+            continue
+
+    glEnd()
 
 def DisplayGL():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-    glRotatef(2,1,3,0)
-    LSystem()
+    #glRotatef(2,1,3,0)
+    LSystem(axiom, rules, recursion)
     glutSwapBuffers()
  
 def timer(i):
@@ -16,19 +168,23 @@ def timer(i):
     glutTimerFunc(50,timer,1)
 
 # PROGRAMA PRINCIPAL
-print("0: Desenha uma linha que termina em uma folha")
-print("1: Desenha uma linha")	
-print("[: Move 25 graus para a esquerda")
-print("]: Move 25 graus para a direita")
-axiom    = str(input("Entre com um axioma: "))
-rawRules = str(input("Entre com as regras: "))
-if ',' in rawRules:
-    rules = addRules([rawRules])
-else:
-    rules = addRules(rawRules.split(','))
-rules    = rawRules.split(',')
+print("F: Desenha uma linha")
+print("L: Move para a esquerda")
+print("R: Move para a direita")
+print("U: Move para cima")
+print("D: Move para baixo")	
+print("[: Guarda uma posicao")
+print("]: Volta para a posicao guardada")
+axiom     = raw_input("Entre com um axioma: ")
+rawRules  = raw_input("Entre com as regras: ")
+recursion = input("Entre com a quantidade de vezes que as regras serao aplicadas: ")
 
-input()
+if ',' in rawRules:
+    rules = AddRules(rawRules.split(','))
+else:
+    rules = AddRules([rawRules])
+
+LSystem(axiom, rules, recursion)
 
 glutInit(sys.argv)
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
@@ -38,8 +194,7 @@ glutDisplayFunc(DisplayGL)
 glEnable(GL_MULTISAMPLE)
 glEnable(GL_DEPTH_TEST)
 glClearColor(0.,0.,0.,1.)
-gluPerspective(90,800.0/600.0,0.1,50.0)
+gluPerspective(120,800.0/600.0,0.1,50.0)
 glTranslatef(0.0,0.0,-5)
-glRotatef(45,1,1,1)
 glutTimerFunc(50,timer,1)
 glutMainLoop()
